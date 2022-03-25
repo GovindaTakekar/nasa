0ad89bcaee163dbc151a3552bbd3a94ae99066d8
GovindaTakekar <72185898+GovindaTakekar@users.noreply.github.com>
2022-03-25 12:58:13 +0530

    Update planets.model.js

--- src/models/planets.model.js
+++ src/models/planets.model.js
@@ -1,48 +1,44 @@
 const fs = require("fs");
 const path = require("path");
-const { parse } = require("csv-parse"); 
+const { parse } = require("csv-parse");
 
 const isHabbitablePlanets = [];
 
 function isHabbitable(planet){
-    return planet["koi_disposition"] === "CONFIRMED" && planet["koi_insol"] > 0.36 && planet["koi_insol"] < 1.11 && planet["koi_prad"] < 1.6 ;
+    return planet["koi_disposition"] === "CONFIRMED" && planet["koi_insol"] > 0.36 && planet["koi_insol"] < 1.11;
 }
 
 function loadPlanetsData(){
+    return new Promise((resolve , rejects) => {
+      fs.createReadStream(path.join(__dirname , ".." , ".." , "data" , "kepler_data.csv"))
+        .pipe(parse({
+          comment: '#',
+          columns: true
+       }))
+        .on("data" , (data)=>{
+            if(isHabbitable(data)){
+                isHabbitablePlanets.push({
+                    kepler_name : data['kepler_name'],
+                });
+            }
+        })
+        .on("error" , (err)=>{
+            console.log(err);
+            rejects(err);
+        })
+        .on("end" , ()=>{
+            console.log(`total ${isHabbitablePlanets.length} planets found`);
+            resolve();
+        });
 
-   return new Promise((resolve , reject) => {
-       fs.createReadStream(path.join(__dirname ,"..", ".." ,"data", "kepler_data.csv"))
-         .pipe(parse({
-             comment: "#",
-             columns: true
-         }))
-         .on("data" , (data)=>{
-             if(isHabbitable(data)){
-                 isHabbitablePlanets.push({
-                     kepler_name: data["kepler_name"],
-                     isConfirmed : data["koi_disposition"],
-                     flux : data["koi_insol"],
-                     prad: data["koi_prad"]
-                 });
-             }
-         })
-         .on("error" , (err)=>{
-             console.log(err);
-             reject(err);
-         })
-         .on('end' , ()=>{ 
-             console.log(`the total ${isHabbitablePlanets.length} planet is found !! `);
-             resolve();
-         });
-       
-         parse();
+        parse();
+
 });
-       
 }
 
-  module.exports = {
-      loadPlanetsData,
-      planets : isHabbitablePlanets,
-  }
 
 
+module.exports = {
+    loadPlanetsData,
+    planets : isHabbitablePlanets,
+}
